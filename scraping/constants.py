d366130c2979cef3d46ee267cbce1d732d7c5d85
tharukadevendra <tsdevendra1@gmail.com>
2024-11-20 00:32:41 +0000

    Add constants.py

---
+++ scraping/constants.py
@@ -0,0 +1,19 @@
+# Taken from https://en.wikipedia.org/wiki/Wikipedia:Namespace
+WIKIPEDIA_SUBJECT_NAMESPACES = {
+    "Main:",
+    "User:",
+    "Wikipedia:",
+    "File:",
+    "MediaWiki:",
+    "Template:",
+    "Help:",
+    "Category:",
+    "Portal:",
+    "Draft:",
+    "TimedText:",
+    "Module:",
+    "Gadget:",
+    "Gadget definition:",
+    "Special:",
+    "Media:",
+}
